ff88df9f73dd11c4c36f924ce0f8bbacb2e904ec
weixuanzh <77259685+weixuanzh@users.noreply.github.com>
2021-05-02 21:16:56 +0800

    Updated "FetchData" page

--- Frontend/CS196Test/ClientApp/src/components/FetchData.js
+++ Frontend/CS196Test/ClientApp/src/components/FetchData.js
@@ -1,59 +1,59 @@
 import React, { Component } from 'react';
-
+import './FetchData.css';
+import $ from 'jquery';
 export class FetchData extends Component {
   static displayName = FetchData.name;
 
-  constructor(props) {
-    super(props);
-    this.state = { forecasts: [], loading: true };
+
+  startVideo(event)
+  {
+    const player = document.getElementById('player');
+    const constraints = {video: true,};
+    navigator.mediaDevices.getUserMedia(constraints).then((stream) => {player.srcObject = stream;});
   }
 
-  componentDidMount() {
-    this.populateWeatherData();
+  captureImg(event)
+  {
+    const canvas = document.getElementById('canvas');
+    const context = canvas.getContext('2d');
+    context.clearRect(0, 0, canvas.width, canvas.height);
+    const player = document.getElementById('player');
+    context.drawImage(player, 0, 0, canvas.width, canvas.height);
+    var dataURL = canvas.toDataURL();
+    $.ajax({
+        traditional:true,
+        url:'http://localhost:5000/capture',
+        type:"POST",
+        data:dataURL,
+        success:function(response) {document.getElementById('feedback').innerHTML = response},
+        error:function(response) {document.getElementById('feedback').innerHTML = 'an error occured. Please try again'}
+        });
   }
 
-  static renderForecastsTable(forecasts) {
-    return (
-      <table className='table table-striped' aria-labelledby="tabelLabel">
-        <thead>
-          <tr>
-            <th>Date</th>
-            <th>Temp. (C)</th>
-            <th>Temp. (F)</th>
-            <th>Summary</th>
-          </tr>
-        </thead>
-        <tbody>
-          {forecasts.map(forecast =>
-            <tr key={forecast.date}>
-              <td>{forecast.date}</td>
-              <td>{forecast.temperatureC}</td>
-              <td>{forecast.temperatureF}</td>
-              <td>{forecast.summary}</td>
-            </tr>
-          )}
-        </tbody>
-      </table>
-    );
-  }
+
 
   render() {
-    let contents = this.state.loading
-      ? <p><em>Loading...</em></p>
-      : FetchData.renderForecastsTable(this.state.forecasts);
-
     return (
-      <div>
-        <h1 id="tabelLabel" >Weather forecast</h1>
-        <p>This component demonstrates fetching data from the server.</p>
-        {contents}
-      </div>
-    );
-  }
+    <div>
+        <h1>fetch data</h1>
+        <div class = 'videoPlayer'>
+            <video id="player" controls autoPlay></video>
+        </div>
 
-  async populateWeatherData() {
-    const response = await fetch('weatherforecast');
-    const data = await response.json();
-    this.setState({ forecasts: data, loading: false });
+        <div class='preview'>
+            <h3>image preview</h3>
+            <canvas id="canvas" width='320' height='240'></canvas>
+            <div>
+                <b>Feedback: </b>
+                <b id='feedback'> No input provided</b>
+            </div>
+        </div>
+
+        <div class = 'start button'>
+            <button onClick={this.startVideo}>Enable your webcam</button>
+            <button onClick={this.captureImg}> capture an image</button>
+        </div>
+    </div>
+    );
   }
 }
