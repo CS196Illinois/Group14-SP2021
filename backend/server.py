34ef55b7c5b2e67843c2320305f28c94ff1623ba
weixuanzh <77259685+weixuanzh@users.noreply.github.com>
2021-05-02 21:08:01 +0800

    flask server able to predict image posted by webcam

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,7 @@
 import base64
 import urllib
 from io import BytesIO
-
+from flask_cors import CORS
 from flask import Flask, render_template, jsonify, request
 import tensorflow as tf
 import cv2
@@ -10,60 +10,51 @@ from PIL import Image
 from binascii import a2b_base64
 
 app = Flask(__name__)
-
+CORS(app)
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/hello', methods=['GET', 'POST'])
-def hello():
+@app.route('/upload', methods=['GET', 'POST'])
+def upload():
+    if request.method == 'POST':
+        img = request.form.get("photo")
+        print(request)
+        return 'OK', 200
+
+
+@app.route('/capture', methods=['POST'])
+def capture():
     # POST request
     if request.method == 'POST':
         # get the encoded image sent by POST request
         print('Incoming..')
         data = request.form.keys()
-        count = 0
         data_url = None
         for x in data:
             data_url = x
+        # preprocess encoded string so that python can decode it correctly
         header, encoded = data_url.split(",", 1)
-        print(encoded)
-        # decode the posted image, not finished
-        decoded_img = base64.decodebytes(bytes(encoded + '==', 'utf-8'))
-        with open("image.png", "wb") as f:
-            f.write(decoded_img)
-        return 'OK', 200
-
-    # GET request
-    else:
-        message = {'greeting': 'Hello from Flask!'}
-        return jsonify(message)
-
-
-@app.route('/my-link/')
-# used for testing
-def my_link():
-    print('I got clicked!')
-    model_dir = r'E:\a CS196 group project\neural network\CNN saved models\model 1'
-    vc = cv2.VideoCapture(0)
-    ret, captured = vc.read()
-    if ret:
-        temp = Image.fromarray(captured, 'RGB')
+        another = encoded.replace(' ', '+')
+        # decode the posted image
+        decoded_img = base64.decodebytes(bytes(another, 'utf-8') + b'===')
+        # create an Image instance based on decoded image
+        i = Image.open(BytesIO(decoded_img))
+        # load captured image into CNN model
+        model_dir = r'E:\a CS196 group project\neural network\CNN saved models\model 1'
+        temp = Image.fromarray(np.asarray(i), 'RGB')
         resized_img = temp.resize((1024, 1024))
         img_input = np.asarray(resized_img)
         # loads trained model and predict the captured image.
         model = tf.keras.models.load_model(model_dir)
         result = model.predict_classes(np.asarray([img_input]))
+        classes = ['correctly', 'incorrectly']
         print(result)
-        if result[0] == 1:
-            return "incorrectly"
-        else:
-            return "correctly"
+        return classes[result[0]], 200
     else:
-        print('failed to capture image!')
-        return "nothing"
+        return 'Bad Request', 405
 
 
 if __name__ == '__main__':
